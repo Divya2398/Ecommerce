4b9e46dc37180b44400e942b0871cb2a6c50f33a
Divya2398 <102854040+Divya2398@users.noreply.github.com>
2022-05-13 15:33:39 +0530

    Add files via upload

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,16 @@
-
 const router = require('express').Router();
+const multer = require('multer');
+const xlsx = require('xlsx');
+
+
 const productSchema = require('../models/product.model')
 const {producttestSchema} = require('../datavalidation/joi')
+const categorySchema = require('../models/category.model')
 const {Admin} = require('../middleware/auth');
 const { authverify } = require('../middleware/auth')
+const store = require("../middleware/multer");
+const path =require('path');
+const upload = multer({storage : store.storage});
 
 
 router.post('/add-product',Admin, async(req,res)=>{
@@ -40,7 +47,7 @@ router.put('/update-product',Admin , async(req,res)=>{
     }
 })
 
-router.get('/get-allproduct', authverify , async(req,res)=>{
+router.get('/get-allproduct',async(req,res)=>{
     try {
         products = await productSchema.find().exec();
         if(products){
@@ -49,6 +56,53 @@ router.get('/get-allproduct', authverify , async(req,res)=>{
     } catch (error) {
         console.log(error.message);
         return res.status(400).json({"status": 'failure', 'message': error.message})
+    }
+})
+
+//category based product
+router.get('/cat-based-product', async(req,res)=>{
+    try {
+        productdetail = await categorySchema.aggregate([
+            {
+                $match:{
+                    uuid:req.query.uuid
+                }
+            },
+            {
+                "$lookup":{
+                    from:"products",
+                    localField:"uuid",
+                    foreignField:"categoryuuid",
+                    as:"product"
+                },
+            },
+            {
+                
+                    $unwind:{    
+                        path:"$product",
+                        preserveNullAndEmptyArrays:true
+                    }
+                
+            },
+            {
+                $project:{
+                    "_id":0,
+                    "categoryName":1,
+                    "product.productName":1,
+                    "product.Description":1,
+                    "product.price":1
+                 }
+            }
+        ])
+        if(productdetail){
+            return res.status(200).json({"status":"success","message":"products fetched successfully ", "result":productdetail})
+        }else{
+            return res.status(400).json({"status":"no product found under this category"})
+        }
+        
+    } catch (error) {
+        console.log(error.message);
+        return res.status(400).json({"status": 'failure', 'message': error.message}) 
     }
 })
 //filter
@@ -82,6 +136,7 @@ router.get('/filter-price',authverify, async(req,res)=>{
         return res.status(400).json({"status": 'failure', 'message': error.message})  
     }
 })
+
 router.get('/search-productname',authverify, async(req,res)=>{
     try {
         const find_product = await productSchema.findOne({productName : req.query.productName}).exec()
@@ -96,7 +151,7 @@ router.get('/search-productname',authverify, async(req,res)=>{
     }
 })
 
-router.get("/search-product",authverify, async (req, res) => {
+router.get("/search-product", async (req, res) => {
     // create  object to hold search value 
     const find_product = {};
     // assign search value to find_product.productName
@@ -115,7 +170,36 @@ router.get("/search-product",authverify, async (req, res) => {
         return res.status(500).json('Error to get products', error.message)
     }
 })
- 
+// read bulkdata and upload bulk data in db
+router.post("/bulk-upload",upload.single('file'), async(req,res)=>{
+    try {
+        let path = './uploads/'+ req.file.filename;
+        console.log("path=", path)
+        let datas = xlsx.readFile(path);
+        let sheetname = datas.SheetNames
+        console.log("sheetname=", sheetname)
+        console.log("_".repeat(100))
+        let resultdata = xlsx.utils.sheet_to_json(datas.Sheets[sheetname[0]]);
+        //console.log(resultdata)
+        console.log("".repeat(100))
+        for(const x of resultdata){
+            //console.log(x)
+            console.log("".repeat(100))
+            const finddata = await productSchema.findOne({productName:x.productName})
+            if(finddata){
+                updatedata = await productSchema.findOneAndUpdate({productName:x.productName},{quantity:finddata.quantity+x.quantity},{new:true})
+                //console.log("product already exist")
+            }else{
+            const data = new productSchema(x);
+            const result = await data.save();
+            console.log(result)
+        }
+         }
+        return res.status(200).json({"status":"success","message":" upload process completed"})
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({status: "failure", message: error.message}) 
+    }
+})
 module.exports = router;
-
 
